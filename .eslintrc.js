f37eaf1ad2aab18db5fd6b046ed549f0c6494112
Xiaowei Xue <xiaowei.xue@shopee.com>
2023-08-19 23:05:50 +0800

    chore: command for add package in monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,8 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  overrides: [
-    {
-      files: ['*.ts'],
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-      },
-      plugins: ['@typescript-eslint'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      rules: {
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
-  ],
-  plugins: ['node', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-    'node/no-missing-import': 'off',
-    'node/no-unsupported-features/es-syntax': 'off',
-    'node/no-missing-require': 'off',
-    'node/shebang': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    quotes: ['warn', 'single', { avoidEscape: true }],
-    'node/no-unpublished-import': 'off',
-  },
+  extends: ['cnp'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.eslint.json'
+  }
 };
